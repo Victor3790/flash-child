c48499b0a523420d352eb8299cce192a70fd01cf
victor <victor182@msn.com>
2019-05-23 18:02:57 -0500

    Child theme optimized

--- functions.php
+++ functions.php
@@ -1,40 +1,53 @@
 <?php
-//Add the parent theme styles and he child theme styles
-
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+//Add the parent theme styles and the child theme styles
 
 function my_theme_enqueue_styles() {
 
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'chlid-style', get_stylesheet_directory_uri() . '/css/flash_child_custom.css' );
-    wp_enqueue_style( 'swiper-style', get_template_directory_uri() . '/css/swiper.min.css');
-    wp_enqueue_style( 'custom_font', 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700');
-    wp_enqueue_style( 'custom_font_header', 'https://fonts.googleapis.com/css?family=Oswald:700&display=swap');
-    wp_enqueue_script( 'swiper-script', get_template_directory_uri() . '/js/swiper.jquery.min.js', array('jquery'), '1.0.0', true);
-    wp_enqueue_script('flash-child-script', get_stylesheet_directory_uri() . '/js/flash_child_custom.js', array('jquery'),'1.0.0', true);
-    wp_enqueue_script('youtube-script', get_stylesheet_directory_uri() . '/js/youtube.js', array('jquery'),'1.0.0', true);
-    wp_enqueue_script('flash-child-parallax-script', get_stylesheet_directory_uri() . '/js/jquery.enllax.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_style( 'chlid-style', get_stylesheet_directory_uri() . '/css/flash_child.css' );
+
+    if(is_front_page()){
+      wp_enqueue_style( 'chlid-home-style', get_stylesheet_directory_uri() . '/css/flash_child_home.css' );
+      wp_enqueue_script('youtube-script', get_stylesheet_directory_uri() . '/js/youtube.js', array(),'1.0.0', true);
+      wp_enqueue_script('flash-child-parallax-script', get_stylesheet_directory_uri() . '/js/jquery.enllax.min.js', array('jquery'), '1.0.0', true);
+      wp_enqueue_script('flash-child-script', get_stylesheet_directory_uri() . '/js/flash_child_home.js', array('jquery'),'1.0.0', true);
+    }
 }
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 
 //Copyright information
-
-if ( ! function_exists( 'child_flash_footer_copyright' ) ) :
-/**
- * Footer Copyright Text.
- *
- * @since Flash 1.0
- */
-function child_flash_footer_copyright() {
-	?>
-<div class="copyright">
-	<span class="copyright-text">
-		<?php printf( esc_html__( 'Copyright %1$s %2$s', 'flash' ), '&copy; ', date( 'Y' ) ); ?>
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo get_bloginfo( 'name' ); ?></a>
-	</span>
-</div><!-- .copyright -->
-<?php
+function flash_footer_copyright() {
+  ?>
+    <div class="copyright">
+    	<span class="copyright-text">
+    		<?php printf( esc_html__( 'Copyright %1$s %2$s', 'flash' ), '&copy; ', date( 'Y' ) ); ?>
+    		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo get_bloginfo( 'name' ); ?></a>
+    	</span>
+    </div><!-- .copyright -->
+  <?php
 }
-endif;
 
-add_action( 'child_flash_copyright_area', 'child_flash_footer_copyright' );
+add_action( 'flash_copyright_area', 'flash_footer_copyright' );
+
+//Custom Google fonts
+function flash_fonts_url() {
+
+  $fonts_url = '';
+  $fonts     = array();
+  $subsets   = 'latin,latin-ext';
+
+  $fonts[] = get_theme_mod( 'flash_body_font', 'Oswald:700|Source+Sans+Pro:700&display=swap' );
+
+  if ( $fonts ) {
+    $fonts_url = add_query_arg( array(
+      'family' => urlencode( implode( '|', $fonts ) ),
+      'subset' => urlencode( $subsets ),
+    ), 'https://fonts.googleapis.com/css' );
+  }
+
+  return $fonts_url;
+}
+
+add_action( 'wp_enqueue_scripts', 'flash_fonts_url');
